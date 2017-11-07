8c9831a583bf13610349981b4af310dee22cb0a6
montygoldy <montygoldy2006@gmail.com>
2017-11-07 00:08:09 +0000

    Added authentication as well as module for access token

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,22 @@
-var request = require('request');
+var secrets = require('./secret'); // Requiring secret module containing key
+var request = require('request'); // Request module
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+function getRepoContributors(repoOwner, repoName, callback){
+  var options = {
+    url: "https://api.github.com/" + repoOwner + "/" + repoName + "/contributors",
+    header: {
+      'User-Agent': 'request',
+      'Authorization': 'GITHUB_TOKEN'
+    }
+  }
+
+  request(options, function(err, response, body){
+    callback(err, body);
+  });
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
